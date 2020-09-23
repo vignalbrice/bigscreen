a2b5ef6b75c5095255088fa629149ec178d620e9
Vignal Brice <vignal.brice@gmail.com>
2020-09-23 04:15:07 +0200

    response footer changed

--- resources/js/screens/Surveys/Surveys.js
+++ resources/js/screens/Surveys/Surveys.js
@@ -75,7 +75,7 @@ const Surveys = () => {
                 Swal.fire({
                     title: "Good Job !",
                     html: `<p>${response.data.text}</p>`,
-                    footer: `<a href="${PROD_URL}/${response.data.url}">Voir mes réponses</a>`,
+                    footer: `<a href="/reponses/${response.data.url}">Voir mes réponses</a>`,
                     icon: "success",
                     showConfirmButton: false,
                     showCancelButton: false,
