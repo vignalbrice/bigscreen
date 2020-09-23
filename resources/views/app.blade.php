576d6e0bb6ad866fbbb2833aa191f71b154631fd
Vignal Brice <vignal.brice@gmail.com>
2020-09-23 03:59:54 +0200

    changed app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,9 +11,9 @@
     <!-- Styles -->
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     <!-- Icone -->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset(images/apple-touch-icon.png) }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ secure_asset(images/favicon-32x32.png) }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset(images/favicon-16x16.png) }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('images/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ secure_asset('images/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('images/favicon-16x16.png') }}">
 </head>
 
 <script>
